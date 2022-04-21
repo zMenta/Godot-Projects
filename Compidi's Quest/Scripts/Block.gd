56c5a95765959c1857f5af6bc4a3183ff4b19d2d
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-04-21 18:59:11 -0300

    Compidi's quest

--- Compidi's Quest/Scripts/Block.gd
+++ Compidi's Quest/Scripts/Block.gd
@@ -1,20 +1,24 @@
 extends StaticBody2D
 
+signal point_made
+
 var screen_size 
 var rect_size_x
 
+
 func _ready() -> void:
+	# Signal connection
+	connect("point_made", owner, "_on_point_made")
+	
+	# Making the block color reach the end of the screen.
 	screen_size = get_viewport_rect().size
 	rect_size_x = $ColorRect.get_size().x
 	
 	$ColorRect.set_size(Vector2(rect_size_x , screen_size.y))
 
 
-
-func _on_Area2D_body_shape_entered(body_rid: RID, body: Node, body_shape_index: int, local_shape_index: int) -> void:
-	print("body entered:", body)
-	
+func _on_Area2D_body_entered(body: Node) -> void:
 	yield(get_tree().create_timer(2),"timeout")
 	
-	print(body)
-	
+	if $Area2D.overlaps_body(body):
+		emit_signal("point_made")
--- Compidi's Quest/Scripts/Player.gd
+++ Compidi's Quest/Scripts/Player.gd
@@ -1,16 +1,10 @@
 extends RigidBody2D
 
 
-# Declare member variables here. Examples:
-# var a: int = 2
-# var b: String = "text"
-
 var direction : Vector2
 var velocity := 0
 
-func _ready() -> void:
-	pass
-		
+
 func _process(delta: float) -> void:
 	
 	#Animation
@@ -21,7 +15,6 @@ func _process(delta: float) -> void:
 		
 	#Controls
 	if Input.is_action_just_pressed("m1_click"):
-		print(get_local_mouse_position().normalized())
 		direction = get_local_mouse_position().normalized()
 	if Input.is_action_pressed("m1_click"):
 		velocity += 10
@@ -30,5 +23,3 @@ func _process(delta: float) -> void:
 		linear_velocity += direction * $ProgressBar.value
 		velocity = 0	
 		$ProgressBar.value = 0
-		
-	
