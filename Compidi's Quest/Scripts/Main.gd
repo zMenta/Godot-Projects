ccb8f01a498e17789cd72ad7c3c2c3dca94ad2ff
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-04-22 17:28:33 -0300

    compidis quest | spawning blocks WIP

--- Compidi's Quest/Scripts/Main.gd
+++ Compidi's Quest/Scripts/Main.gd
@@ -1,9 +1,21 @@
 extends Node
 
+export(PackedScene) var block_scene
+
+func _ready() -> void:
+	randomize()
+
+func spawn_block():
+	var block = block_scene.instance()	
+	var block_spawn_location = $BlockPath/BlockSpawnPosition
+	
+	block_spawn_location.offset = randi()
+	block.position = block_spawn_location.position
+
+	add_child(block)
+
 func _on_point_made():
+	spawn_block()
 	print("point made!")
 	$Player.set_sleeping(true)
 	
-
-func _on_player_jump(speed_x) -> float:
-	return speed_x
