6a514429fd51066c44600cb6b136c6716b5d6620
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-04-22 17:05:06 -0300

    compidis's quest progress | camera

--- Compidi's Quest/Scripts/Main.gd
+++ Compidi's Quest/Scripts/Main.gd
@@ -2,14 +2,7 @@ extends Node
 
 func _on_point_made():
 	print("point made!")
-	
 	$Player.set_sleeping(true)
-
-onready var player = $Player
-
-func _physics_process(delta: float) -> void:
-	print(player.linear_velocity)
-		
 	
 
 func _on_player_jump(speed_x) -> float:
