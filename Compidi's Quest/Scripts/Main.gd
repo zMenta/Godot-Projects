73e740bda397699ef93671fea7a025d84dc1d7f8
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2022-04-24 19:27:24 -0300

    CQ | Minor spawning block distance tweak

--- Compidi's Quest/Scripts/Main.gd
+++ Compidi's Quest/Scripts/Main.gd
@@ -26,7 +26,7 @@ func spawn_block():
 	var block = block_scene.instance()
 	var block_spawn_location = $BlockPosition
 	var y_random_position := int(rand_range(-100, 100))
-	var x_random_position := int(rand_range(250, 320))
+	var x_random_position := int(rand_range(250, 300))
 	
 	block.position = block_spawn_location.position
 	block_spawn_location.position += Vector2(x_random_position, y_random_position)
