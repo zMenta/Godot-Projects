a1849e55f4b231003bf8f0bcdc2e941a0f317fab
Lucas V. Moog Brentano <70714721+zMenta@users.noreply.github.com>
2022-05-13 21:10:11 -0300

    Alien Movement with "New_position"

--- Invaders from Space/Scripts/Main.gd
+++ Invaders from Space/Scripts/Main.gd
@@ -1,8 +1,8 @@
 extends Node
 
 
-func _process(delta: float) -> void:
-	$UfoPath/PathFollow2D.offset += 250 * delta
+#func _process(delta: float) -> void:
+#	$UfoPath/PathFollow2D.offset += 250 * delta
 	
 func on_Player_hit():
 	$Player.death()
--- Invaders from Space/Scripts/Ufo.gd
+++ Invaders from Space/Scripts/Ufo.gd
@@ -2,14 +2,24 @@ extends Area2D
 
 export (PackedScene) var Bullet 
 
-var screen = OS.get_window_safe_area().size
+var screen := OS.get_window_safe_area().size
+var speed := 100
+var velocity := Vector2.ZERO
+onready var new_position := position
 
-func _ready():
-	print(screen)
 
 func _physics_process(delta):
-	position.x = clamp(position.x, 0, screen.x)
+	position.x = clamp(position.x, 0+60, screen.x-60)
 	position.y = clamp(position.y, 0, screen.y)
+	if new_position != position:
+		var direction = position.direction_to(new_position)
+		var lenght = (position - new_position).length()
+		if lenght > 1:
+			velocity = direction * speed * delta
+		else:
+			velocity = Vector2.ZERO
+		position += velocity
+		
 
 
 func death():
