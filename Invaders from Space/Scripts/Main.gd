c61ca9b807d20087cebb2338832ed2aa3b20bf93
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-05-17 12:17:54 -0300

    Aliens from Space | Ufo spawning, movement. And simple ui  stats

--- Invaders from Space/Scripts/Main.gd
+++ Invaders from Space/Scripts/Main.gd
@@ -2,13 +2,15 @@ extends Node
 
 export (PackedScene) var Ufo
 var score := 0
+var seconds := 0
+var minutes := 0
+var player_alive := true
+var enemy_list := []
+
 
 func _ready():
 	randomize()
 
-
-#func _process(delta: float) -> void:
-#	pass
 
 func spawn_alien():
 	var Alien = Ufo.instance()
@@ -18,19 +20,29 @@ func spawn_alien():
 
 	Alien.position = alien_spawn_path.position
 
+	enemy_list.append(Alien)
 	add_child(Alien)
 	Alien.create_new_position()
 
 
 func on_Player_hit():
 	$Player.death()
-
+	player_alive = false
+	
 
 func on_Alien_hit(alien):
 	score += 1
-	$Hud/Score.text = str(score)
+	$Hud/Score.text = "Kills: " + str(score)
 	alien.death()
+	enemy_list.erase(alien)
 
 
 func _on_AlienSpawnTimer_timeout():
-	spawn_alien()
+	seconds += 1
+	if player_alive == true:
+		$Hud/Time.text = "Time - " + str(minutes) + " : " + str(seconds)	
+	if seconds >= 60:
+		minutes += 1
+		seconds = 0
+	if enemy_list.size() < 30:
+		spawn_alien()
