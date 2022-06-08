85ef3e75905f2f7f98a6f3158087d6fb79af3146
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-08 14:49:37 -0300

    Zombover | WeaponBase Spread/Recoil

--- OverDose/Weapons/WeaponBase.gd
+++ OverDose/Weapons/WeaponBase.gd
@@ -11,11 +11,25 @@ onready var animation_player := $AnimationPlayer
 export(PackedScene) var Bullet
 export var bullet_damage := 10
 export var bullet_direction := Vector2.ZERO
+export var max_recoil_angle := 5.0 # Both Axis. 5 + 5 degrees = 10 degress total
+export var min_recoil_angle := 2.0
+export (float, 1) var recoil_climb_weight := 0.1
+export (float, 1) var recoil_recovery_weight := 0.05
+
+
+var current_recoil := min_recoil_angle
 
 
 func fire() -> void:
 	if cooldown_timer.is_stopped():
-		bullet_direction = handle.global_position.direction_to(muzzle.global_position)
+		var recoil_radians = deg2rad(rand_range(-current_recoil, current_recoil))
+		bullet_direction = handle.global_position.direction_to(muzzle.global_position).rotated(recoil_radians)
+		
+		# Recoil / spread
+		var recoil_increment := max_recoil_angle * recoil_climb_weight
+		current_recoil = clamp(current_recoil + recoil_increment, min_recoil_angle, max_recoil_angle)
+		
+		
 		cooldown_timer.start()
 		animation_player.play("MuzzleFlash")
 		GlobalSignals.emit_signal("bullet_fired", Bullet.instance() ,bullet_direction, muzzle.global_position)
