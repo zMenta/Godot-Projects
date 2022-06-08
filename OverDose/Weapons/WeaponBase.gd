d6f0b10e06879d0adbf030458e79c869b0e43d1d
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-08 11:39:00 -0300

    Zombover | WeaponBase gun flash

--- OverDose/Weapons/WeaponBase.gd
+++ OverDose/Weapons/WeaponBase.gd
@@ -5,6 +5,7 @@ class_name Weapon
 onready var muzzle := $Muzzle
 onready var handle := $Handle
 onready var cooldown_timer := $FireCooldownTimer
+onready var animation_player := $AnimationPlayer
 
 
 export(PackedScene) var Bullet
@@ -12,12 +13,10 @@ export var bullet_damage := 10
 export var bullet_direction := Vector2.ZERO
 
 
-func _ready() -> void:
-	bullet_direction = handle.global_position.direction_to(muzzle.global_position)
-
-
 func fire() -> void:
 	if cooldown_timer.is_stopped():
+		bullet_direction = handle.global_position.direction_to(muzzle.global_position)
 		cooldown_timer.start()
+		animation_player.play("MuzzleFlash")
 		GlobalSignals.emit_signal("bullet_fired", Bullet.instance() ,bullet_direction, muzzle.global_position)
 
