760e4d7f397b009a34752690e2c688f8ff05fd24
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-09 14:13:01 -0300

    Zombover | Zombie Pathfinding line 2d

--- OverDose/Actors/AI.gd
+++ OverDose/Actors/AI.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+onready var line2d := $PathFindingLine
 
 var movement = null
 var direction := Vector2.ZERO
@@ -8,6 +9,7 @@ var player: KinematicBody2D = null
 
 
 func _ready() -> void:
+	yield(get_tree(), "idle_frame")
 	if get_tree().get_nodes_in_group("navigation").size() > 0:
 		navigation = get_tree().get_nodes_in_group("navigation")[0]
 		
@@ -16,6 +18,7 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	line2d.global_position = Vector2.ZERO
 	if navigation != null and player != null:
 		update_direction()
 		movement.direction = direction
@@ -26,6 +29,7 @@ func initialize(movement_node) -> void:
 
 
 func update_direction() -> void:
-	var path := navigation.get_simple_path(global_position, player.global_position)
+	var path := navigation.get_simple_path(global_position, player.global_position, false)
+	line2d.points = path
 	if path.size() > 0:
 		direction = global_position.direction_to(path[1])
