089063acc541094af100af262b46249a2b4c0c36
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-09 17:49:30 -0300

    Zombover | WeaponBase MaxAmmo and reload function

--- OverDose/Weapons/WeaponBase.gd
+++ OverDose/Weapons/WeaponBase.gd
@@ -17,22 +17,24 @@ export var min_recoil_angle := 2.0
 export (float, 1) var recoil_climb_weight := 0.1
 export (float, 1) var recoil_recovery_weight := 0.05
 export var magazine_size := 10
+export var max_ammo := 100
 
 
 onready var current_magazine_bullet_count = magazine_size
+onready var current_max_ammo = max_ammo
 var current_recoil := min_recoil_angle
 var bullet_direction := Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	print(current_magazine_bullet_count)
+	print(current_magazine_bullet_count, " | ", current_max_ammo)
 	if not Input.is_action_pressed("shoot"):
 		var recoil_decrement := max_recoil_angle * recoil_recovery_weight
 		current_recoil = clamp(current_recoil - recoil_decrement, min_recoil_angle, max_recoil_angle)
 
 
 func reload() -> void:
-	if reload_timer.is_stopped():
+	if reload_timer.is_stopped() and max_ammo > 0:
 		reload_timer.start()
 
 
@@ -62,4 +64,13 @@ func fire() -> void:
 
 
 func _on_ReloadTimer_timeout() -> void:
-	current_magazine_bullet_count = magazine_size
+	if current_max_ammo == 0:
+		return
+	
+	var ammo_amount = magazine_size - current_magazine_bullet_count
+	if current_max_ammo < magazine_size:
+		ammo_amount -= current_max_ammo
+	current_magazine_bullet_count += ammo_amount
+	current_max_ammo -= ammo_amount
+		
+	current_max_ammo = clamp(current_max_ammo, 0, max_ammo)
