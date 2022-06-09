0ba9fc8a19de6762273ce33b6873df54a8f991e2
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-09 13:27:18 -0300

    Zombover | Zombie Navigation

--- OverDose/Actors/AI.gd
+++ OverDose/Actors/AI.gd
@@ -3,10 +3,23 @@ extends Node2D
 
 var movement = null
 var actor = null
+var navigation: Navigation2D = null
 
 
 func initialize(movement_node) -> void:
 	movement = movement_node
-	
+
+
+func _ready() -> void:
+	if get_tree().get_nodes_in_group("navigation").size() > 0:
+		navigation = get_tree().get_nodes_in_group("navigation")[0]
+
+
 func _physics_process(delta: float) -> void:
-	movement.direction = Vector2.RIGHT
+	if navigation != null:
+		movement.direction = Vector2.RIGHT
+	
+	
+func update_direction() -> void:
+	pass
+#	var path := navigation.get_simple_path(global_position, ) 
