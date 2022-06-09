a329d544aab7f6a1567866a5d9dd89e667004262
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-09 13:35:49 -0300

    Zombover | Zombie Basic pathfinding

--- OverDose/Actors/AI.gd
+++ OverDose/Actors/AI.gd
@@ -2,24 +2,30 @@ extends Node2D
 
 
 var movement = null
-var actor = null
+var direction := Vector2.ZERO
 var navigation: Navigation2D = null
-
-
-func initialize(movement_node) -> void:
-	movement = movement_node
+var player: KinematicBody2D = null
 
 
 func _ready() -> void:
 	if get_tree().get_nodes_in_group("navigation").size() > 0:
 		navigation = get_tree().get_nodes_in_group("navigation")[0]
+		
+	if get_tree().get_nodes_in_group("player").size() > 0:
+		player = get_tree().get_nodes_in_group("player")[0]
 
 
 func _physics_process(delta: float) -> void:
-	if navigation != null:
-		movement.direction = Vector2.RIGHT
+	if navigation != null and player != null:
+		update_direction()
+		movement.direction = direction
 	
-	
+
+func initialize(movement_node) -> void:
+	movement = movement_node
+
+
 func update_direction() -> void:
-	pass
-#	var path := navigation.get_simple_path(global_position, ) 
+	var path := navigation.get_simple_path(global_position, player.global_position)
+	if path.size() > 1:
+		direction = global_position.direction_to(path[1])
