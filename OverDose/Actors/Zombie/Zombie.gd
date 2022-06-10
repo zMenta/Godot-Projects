edca0e8ac68bcea6b59ea6bfbae7faa9f70a691e
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-10 20:36:21 -0300

    Zombover | Basic Zombie taking damage and dying

--- OverDose/Actors/Zombie/Zombie.gd
+++ OverDose/Actors/Zombie/Zombie.gd
@@ -5,5 +5,23 @@ onready var ai := $AI
 onready var movement := $Movement
 
 
+export var health := 100
+
+
 func _ready() -> void:
 	ai.initialize(movement)
+
+
+func take_damage(damage: float) -> void:
+	health -= damage
+	if health <= 0:
+		death()
+
+
+func death() -> void:
+	queue_free()
+
+
+func _on_BulletHitArea_area_entered(area: Area2D) -> void:
+	if area is Bullet:
+		take_damage(area.damage)
