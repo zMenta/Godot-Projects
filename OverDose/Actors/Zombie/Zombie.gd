ad7a0865cd3b652de6fa3805402688a0be455a79
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-11 23:16:34 -0300

    Zombover | Zombie Attack CoolDown

--- OverDose/Actors/Zombie/Zombie.gd
+++ OverDose/Actors/Zombie/Zombie.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 
 onready var ai := $AI
 onready var movement := $Movement
+onready var hurt_zone := $HurtZone
+onready var attack_timer := $AttackTimer
+onready var attack_cooldown_timer := $AttackCoolDownTimer
 
 
 export var health := 100
@@ -29,4 +32,13 @@ func _on_BulletHitArea_area_entered(area: Area2D) -> void:
 
 func _on_HurtZone_body_entered(body: Node) -> void:
 	if body.has_method("on_zombie_hit"):
-		body.on_zombie_hit()
+		attack_timer.start()
+
+
+func _on_AttackTimer_timeout() -> void:
+	var bodies : Array = hurt_zone.get_overlapping_bodies()
+	if bodies.size() > 0:
+		attack_timer.start()
+		if attack_cooldown_timer.is_stopped():
+			bodies[0].on_zombie_hit()
+			attack_cooldown_timer.start()
