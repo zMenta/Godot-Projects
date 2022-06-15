681d5e1d82036f76c686c54be60031b25d814db0
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-15 08:59:08 -0300

    Zombover | Base Inventory

--- OverDose/Actors/Player/Inventory.gd
+++ OverDose/Actors/Player/Inventory.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
+export (PackedScene) var default_weapon 
 
-enum inventory {
+
+enum {
 	SLOT1,
 	SLOT2
 }
 
 
+var weapon_slots := {
+	SLOT1: null,
+	SLOT2: null
+}
+
+
+func _ready() -> void:
+	set_weapon(default_weapon, SLOT1)
+
+
+func set_weapon(weapon: Weapon, slot: int) -> void:
+	weapon_slots[slot] = weapon
+	
 
 
