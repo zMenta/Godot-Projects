98410156ea31f27ef2ad63d114007e901db19066
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-23 11:12:26 -0300

    Zombover | Shotgun Scene and Script

--- OverDose/Weapons/WeaponBase/WeaponShotgun.gd
+++ OverDose/Weapons/WeaponBase/WeaponShotgun.gd
@@ -10,11 +10,7 @@ func fire() -> void:
 		spawn_smoke(muzzle.position)
 		spawn_bullet_casing(handle.position)
 		current_magazine_bullet_count -= 1
-		var recoil_radians = deg2rad(rand_range(-current_recoil, current_recoil))
 		
-		# Recoil / spread
-		var recoil_increment := max_recoil_angle * recoil_climb_weight
-		current_recoil = clamp(current_recoil + recoil_increment, min_recoil_angle, max_recoil_angle)
 		
 		
 		# Tween
@@ -28,6 +24,11 @@ func fire() -> void:
 		
 		
 		for pellets in range(pellet_amount):
+			var recoil_radians = deg2rad(rand_range(-current_recoil, current_recoil))
+			# Recoil / spread
+			var recoil_increment := max_recoil_angle * recoil_climb_weight
+			current_recoil = clamp(current_recoil + recoil_increment, min_recoil_angle, max_recoil_angle)
+			
 			bullet_direction = handle.global_position.direction_to(muzzle.global_position).rotated(recoil_radians)
 			GlobalSignals.emit_signal("bullet_fired", Bullet.instance() ,bullet_direction, muzzle.global_position)
 	
