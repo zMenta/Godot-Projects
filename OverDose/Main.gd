75babd0667d0d8366a2018d78cc659b2acbf033e
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-06-23 17:21:11 -0300

    Zombover | Better spacing

--- OverDose/Main.gd
+++ OverDose/Main.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
+
 onready var bullet_manager := $BulletManager
 onready var player := $Player
 onready var GUI := $GUI
 onready var zombie_manager := $ZombieManager
 onready var map := $Map1
 
+
 func _ready() -> void:
 	randomize()
 	GlobalSignals.connect("bullet_fired", bullet_manager, "on_bullet_fired")
 	GUI.initialize(player, zombie_manager)
 	zombie_manager.set_tilemap(map.get_tilemap())
+
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("f11"):
