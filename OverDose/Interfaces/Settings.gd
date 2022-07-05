412410bbff6c7b7ab6c7f5d84f9f5ef4755fde89
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-07-05 12:12:20 -0300

    Zombover | Basic Settings

--- OverDose/Interfaces/Settings.gd
+++ OverDose/Interfaces/Settings.gd
@@ -1,7 +1,15 @@
 extends Control
 
 
+func _ready() -> void:
+	if OS.window_fullscreen:
+		$VBoxContainer/FullScreenCheckBox.pressed = true
+
+
 func _on_Back_pressed() -> void:
 	if MenuGlobal.main_menu != null:
 		get_tree().change_scene_to(MenuGlobal.main_menu)
 	
+
+func _on_FullScreenCheckBox_pressed() -> void:
+	OS.window_fullscreen = not OS.window_fullscreen
