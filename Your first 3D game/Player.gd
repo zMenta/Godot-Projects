6fefb9641edf5782bb2173854e0183f3744ac3a2
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-07-07 15:18:50 -0300

    Your First 3D game | Movement Fix

--- Your first 3D game/Player.gd
+++ Your first 3D game/Player.gd
@@ -1,8 +1,8 @@
 extends KinematicBody
 
-export var speed := 15
-export var jump_force := 20
-export var falling_acceleration := 75
+export var speed := 15.0
+export var jump_force := 20.0
+export var falling_acceleration := 75.0
 
 var velocity := Vector3.ZERO
 
@@ -22,11 +22,14 @@ func _physics_process(delta: float) -> void:
 	
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
-		velocity = move_and_slide(speed * direction, Vector3.UP)
 		$Pivot.look_at(global_transform.origin + direction, Vector3.UP)
 		
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		velocity.y += jump_force
 
-
 	velocity.y -= falling_acceleration * delta
+
+	velocity = move_and_slide(velocity, Vector3.UP)
