ffc0f8d5667046591901dca5bec18dc9f9e11a64
Lucas Brentano <70714721+zMenta@users.noreply.github.com>
2022-07-07 11:34:07 -0300

    3D Ball Game | Change level bugfix

--- 3D Ball Game/assets/Levels/WinZone.gd
+++ 3D Ball Game/assets/Levels/WinZone.gd
@@ -1,6 +1,7 @@
 extends Area
 
+export(String, FILE, "*.tscn") var NEXT_LEVEL = ""
 
 func _on_WinZone_body_entered(body: Node) -> void:
-	if body.name == "Ball":
-		print("you won")
+	if body.name == "Ball" and NEXT_LEVEL != "":
+		get_tree().change_scene(NEXT_LEVEL)
