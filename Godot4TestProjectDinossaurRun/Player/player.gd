2877cdd737f7a93e3dd9a9cf4c1eb6dc166a592b
Lucas Brentano <brentano.l@gmail.com>
2022-10-05 11:04:44 -0300

    Godot4 Dinossaur run prototype. Jumping mechanics

---
+++ Godot4TestProjectDinossaurRun/Player/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const JUMP_VELOCITY := -175
+const MAX_JUMP_VELOCITY := -1200
+
+var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var _can_jump := false
+
+func _physics_process(delta):
+	# Jumping code, the jump will be higher if the jump key be held for longer.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	else:
+		_can_jump = true
+	
+	if Input.is_action_pressed("jump") and _can_jump:
+		velocity.y += JUMP_VELOCITY
+		if velocity.y < MAX_JUMP_VELOCITY:
+			_can_jump = false
+			
+	if Input.is_action_just_released("jump"):
+		_can_jump = false
+
+	move_and_slide()
+
