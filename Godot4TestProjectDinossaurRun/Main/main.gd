7c6d71765fa705ae50772c30a07ddd58ddce7832
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-10-06 20:09:55 -0300

    dino | basic start menu

--- Godot4TestProjectDinossaurRun/Main/main.gd
+++ Godot4TestProjectDinossaurRun/Main/main.gd
@@ -2,10 +2,15 @@ extends Node2D
 
 @onready var level := $level01
 @onready var player : Player = $Player
+@onready var menu := $Menu
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
 	
 	player.died.connect(level._on_Player_death)
+	menu.game_started.connect(_start_game)
+	get_tree().paused = true
 
+func _start_game() -> void:
+	get_tree().paused = false
--- Godot4TestProjectDinossaurRun/Menu/menu.gd
+++ Godot4TestProjectDinossaurRun/Menu/menu.gd
@@ -5,8 +5,11 @@ signal game_started
 
 func _unhandled_key_input(event):
 	if event.is_action_pressed("jump"):
-		game_started.emit()
-
+		emit_start_game()
 
 func _on_start_button_pressed():
+	emit_start_game()
+
+func emit_start_game() -> void:
+	visible = false
 	game_started.emit()
