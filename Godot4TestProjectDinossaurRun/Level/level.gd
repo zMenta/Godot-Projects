660b597d040ad396a111428ff4651893172bc915
Lucas Brentano <brentano.l@gmail.com>
2022-10-06 16:52:14 -0300

    dine | basic spawning obstacles

---
+++ Godot4TestProjectDinossaurRun/Level/level.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var obstacle: PackedScene
+@export var spawn_time_minimum: float = 1.0
+@export var spawn_time_maximum: float = 5.0
+
+@onready var timer : Timer = $Timer
+@onready var ground_obstacle_postition : Marker2D = $GroundObstaclePosition
+@onready var obstacles_node := $Obstacles
+
+
+func _ready() -> void:
+	timer.wait_time = randf_range(spawn_time_minimum, spawn_time_maximum)
+	timer.start()
+	
+# Spawn objects
+func _on_timer_timeout() -> void:
+	timer.wait_time = randf_range(spawn_time_minimum, spawn_time_maximum)
+	
+	var new_obstacle : Obstacle = obstacle.instantiate()
+	new_obstacle.global_position = ground_obstacle_postition.global_position
+	
+	# Random Y size for the obstacles
+	new_obstacle.scale.y = randf_range(1, 3.5)
+	obstacles_node.add_child(new_obstacle)
+	
+	
+	
--- Godot4TestProjectDinossaurRun/Obstacles/obstacle.gd
+++ Godot4TestProjectDinossaurRun/Obstacles/obstacle.gd
@@ -1,10 +1,13 @@
 extends Area2D
+class_name Obstacle
 
 const SPEED := 300
+var is_moving := true
+
 
 func _physics_process(delta):
-	position.x -= SPEED * delta
-
+	if is_moving:
+		position.x -= SPEED * delta
 
 
 func _on_obstacle_body_entered(body):
