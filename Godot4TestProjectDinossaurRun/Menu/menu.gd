04d8439f78d03188c79ac1ed74ee0326f8688398
Lucas V. Moog Brentano <brentano.l@gmail.com>
2022-10-06 21:22:46 -0300

    dino | code order change

--- Godot4TestProjectDinossaurRun/Menu/menu.gd
+++ Godot4TestProjectDinossaurRun/Menu/menu.gd
@@ -25,14 +25,15 @@ func _on_start_button_pressed():
 func emit_start_game() -> void:
 	main_menu.visible = false
 	game_started.emit()
+	
+	if button.text == "Restart":
+		game_restarted.emit()
+
 	if not _game_started:
 		score = 0
 		score_timer.start()
 		_game_started = true	
 		
-	if button.text == "Restart":
-		game_restarted.emit()
-
 
 func change_to_restart_layout() -> void:
 	main_menu.visible = true
