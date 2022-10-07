48f0caed88eb4326bd06cdcf77b4c885132a4fc9
Lucas Brentano <brentano.l@gmail.com>
2022-10-07 17:27:53 -0300

    dino | level speed modifier

--- Godot4TestProjectDinossaurRun/Main/main.gd
+++ Godot4TestProjectDinossaurRun/Main/main.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var player : Player = $Player
 @onready var menu := $Menu
 
+var score := 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	randomize()
--- Godot4TestProjectDinossaurRun/Obstacles/obstacle.gd
+++ Godot4TestProjectDinossaurRun/Obstacles/obstacle.gd
@@ -1,13 +1,13 @@
 extends Area2D
 class_name Obstacle
 
-const SPEED := 300
+var speed := 300
 var is_moving := true
 
 
 func _physics_process(delta):
 	if is_moving:
-		position.x -= SPEED * delta
+		position.x -= speed * delta
 
 
 func _on_obstacle_body_entered(body):
